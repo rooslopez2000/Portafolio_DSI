12db42dc932ee2cb37052726309cdf0036e7cf7e
rooslopez2000 <lopezroos97@gmail.com>
2025-11-08 11:03:03 -0500

    Delete portafolio directory

--- portafolio/js/main.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById("contactForm");
-const msg = document.getElementById("formMsg");
-
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  msg.textContent = "¡Mensaje enviado correctamente!";
-  form.reset();
-});
